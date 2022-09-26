753a772a2a7fab7fc19ea33e8b07af5aaed3dcd9
aw <89423@psdschools.org>
2022-09-26 11:16:51 -0600

    Initial Commit

---
+++ our conscious selfUITests/our_conscious_selfUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  our_conscious_selfUITestsLaunchTests.swift
+//  our conscious selfUITests
+//
+//  Created by Williams, Alexander - Student on 9/26/22.
+//
+
+import XCTest
+
+class our_conscious_selfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
